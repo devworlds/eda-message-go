d756036fd729eaba68ace9592d1e845d2b22e10a
devworlds <jeffersonluiz.dev@gmail.com>
2025-05-09 09:47:02 -0300

    feat: add websocket api base

--- app/cmd/main.go
+++ app/cmd/main.go
@@ -1,8 +1,22 @@
 package main
 
-import "github.com/devworlds/eda-message-go/internal/maths"
+import (
+	//standard
+	"fmt"
+	"log"
+	"net/http"
+
+	//internal
+	"github.com/devworlds/eda-message-go/internal/handlers"
+)
 
 func main() {
-	res := maths.Sum(1, 2)
-	println(res) // Output: 3
+	http.HandleFunc("/ws", handlers.HandleWebSocket)
+
+	fs := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fs)
+
+	port := ":8080"
+	fmt.Printf("Server is running on http://localhost%s\n", port)
+	log.Fatal(http.ListenAndServe(port, nil))
 }
--- app/go.mod
+++ app/go.mod
@@ -1,3 +1,5 @@
 module github.com/devworlds/eda-message-go
 
 go 1.23.3
+
+require github.com/gorilla/websocket v1.5.3 // indirect
