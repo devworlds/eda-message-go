a0b935ef6f24c84699dc2ec5317a2ffff140af97
devworlds <jeffersonluiz.dev@gmail.com>
2025-05-10 21:58:17 -0300

    feat: remove test structs

--- websocket/internal/hub.go
+++ websocket/internal/hub.go
@@ -7,16 +7,14 @@ import (
 	"github.com/gorilla/websocket"
 )
 
-// Update the testBroadcastTrigger and testMessageSent channels to be buffered.
-var TestBroadcastTrigger = make(chan struct{}, 1)
-var TestMessageSent = make(chan struct{}, 1)
-
+// Hub maintains the set of active clients and broadcasts messages to them.
 type Hub struct {
 	Clients   map[*websocket.Conn]bool
 	Broadcast chan []byte
 	Mu        sync.Mutex
 }
 
+// NewHub creates a new Hub instance.
 func NewHub() *Hub {
 	return &Hub{
 		Clients:   make(map[*websocket.Conn]bool),
@@ -24,6 +22,7 @@ func NewHub() *Hub {
 	}
 }
 
+// HandleWebSocket handles WebSocket connections.
 func (h *Hub) Run() {
 	for {
 		msg := <-h.Broadcast
@@ -42,12 +41,14 @@ func (h *Hub) Run() {
 	}
 }
 
+// AddClient adds a new client to the Hub.
 func (h *Hub) AddClient(conn *websocket.Conn) {
 	h.Mu.Lock()
 	h.Clients[conn] = true
 	h.Mu.Unlock()
 }
 
+// RemoveClient removes a client from the Hub.
 func (h *Hub) RemoveClient(conn *websocket.Conn) {
 	h.Mu.Lock()
 	defer h.Mu.Unlock()
